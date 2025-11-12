79f773ca90078fede157792bbabd293d76385a30
missytake <missytake@systemli.org>
2025-11-12 10:43:27 +0100

    fix: pipe into subprocesses correctly

--- pyinfra_wireguard/secrets.py
+++ pyinfra_wireguard/secrets.py
@@ -1,10 +1,11 @@
-import subprocess
+from io import StringIO
+from subprocess import Popen, run, PIPE, STDOUT
 
 
 def get_pass(filename: str) -> str:
     """Get the data from the password manager."""
     try:
-        r = subprocess.run(["pass", "show", filename], capture_output=True)
+        r = run(["pass", "show", filename], capture_output=True)
     except FileNotFoundError:
         print(f"Please install pass and pull the latest version of our pass secrets")
         exit()
@@ -17,12 +18,13 @@ def generate_private_wg_key_locally() -> (str, str):
     :return The private key and the public key as a tuple
     """
     try:
-        privkey = subprocess.run(["wg", "genkey"], capture_output=True).stdout.decode('utf-8').strip()
-        pubkey = subprocess.run(["wg", "pubkey", privkey], capture_output=True).stdout.decode('utf-8').strip()
+        privkey = run(["wg", "genkey"], capture_output=True).stdout
+        pipe = Popen(["wg", "pubkey"], stdout=PIPE, stdin=PIPE, stderr=STDOUT)  # , capture_output=True).stdout.decode('utf-8').strip()
+        pubkey = pipe.communicate(input=privkey)[0]
     except FileNotFoundError:
         print("Can't run `wg genkey`, have you installed wireguard-tools on your local machine?")
         exit(1)
-    return privkey, pubkey
+    return privkey.decode('utf-8').strip(), pubkey.decode('utf-8').strip()
 
 
 def store_public_key_in_pass(pubkey: str, pass_entry: str):
@@ -30,8 +32,9 @@ def store_public_key_in_pass(pubkey: str, pass_entry: str):
     status_quo = get_pass(pass_entry)
     if pubkey != status_quo:
         try:
-            subprocess.run(["echo", pubkey, "|", "pass", "insert", "-e", pass_entry])
+            pipe = Popen(["pass", "insert", "-e", pass_entry], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+            pipe.communicate(input=pubkey.encode('utf-8'))
         except FileNotFoundError:
             print(f"Please install pass and pull the latest version of our pass secrets")
             exit()
-        print(f"Added PublicKey to {pass_entry} - please re-deploy the mother to add the child as a peer.")
+        print(f"Added {pubkey} to {pass_entry} - please re-deploy the mother to add the child as a peer.")
